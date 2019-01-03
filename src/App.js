adfc62492d6d10c3684f7d047094f78ac38ba5cd
Alexander Guevara <warmachine@Alexanders-MacBook-Air.local>
2019-01-02 19:33:52 -0500

    Added weather display component and added small styling changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import WeatherDisplay from './Components/weatherdisplay';
+import './index.css';
 
 class App extends Component {
   constructor(props){
@@ -26,16 +28,19 @@ class App extends Component {
   getResults = async() => {
   // const proxy = "https://cors-anywhere.herokuapp.com/";
   const key = "b01c9406898061c7293d799555d165bf";
-  const call = await axios(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${key}&q=${this.state.city}`);
+  const call = await axios(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${key}&q=${this.state.city}&units=imperial`);
   console.log(call)
  }
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.setCity}>
+        <form onSubmit={this.setCity} className="search">
           <input value = {this.state.city} onChange={this.cityChange}/>
           <button onClick={this.getResults}> Submit </button>
         </form>
+
+        <WeatherDisplay />
+
       </div>
     );
   }
---
+++ src/Components/weatherdisplay.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../index.css'
+
+class WeatherDisplay extends Component{
+	render(){
+		return(
+			<div className="boxContainer"> 
+				<div className="days">
+				</div>
+			</div>
+		);
+	}
+}
+
+export default WeatherDisplay;
